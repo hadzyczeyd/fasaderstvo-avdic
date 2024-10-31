265c338d72c418cc1366dd9d382599baf285e9a6
hadzyczeyd <hadziczejd2008@gmail.com>
2024-10-31 18:44:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.card', { delay: 200 });
+ScrollReveal().reveal('.card2', { delay: 300 });
+ScrollReveal().reveal('.card3', { delay: 500 });
+ScrollReveal().reveal('.naslov1', { delay: 200 });
+ScrollReveal().reveal('.naslov2', { delay: 500 });
